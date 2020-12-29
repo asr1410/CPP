d5698367731dbe2899d7cf66814df79fc250ae5e
asr1410 <abhishek.rawat1410@gmail.com>
2020-12-29 13:30:47 +0530

    change

---
+++ swap_two_number_different.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "**** PROGRAM TO SWAP TO NUMBERS ****" << endl;
+    cout << "Enter the first number" << endl;
+    cin >> num1;
+    cout << "Enter the second number" << endl;
+    cin >> num2;
+
+    //FIRST METHOD TO SWAP
+
+    return 0;
+}
