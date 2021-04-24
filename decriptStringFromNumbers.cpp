76356b3ba6f160ad67ab0b012a7e039cffee2893
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-04-24 14:09:42 +0530

    changes

--- decriptStringFromNumbers.cpp
+++ decriptStringFromNumbers.cpp
@@ -1,38 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solutioin
+string freqAlphabets(string s)
 {
-    string s;
-    int a, b;
-
-public:
-    void getString(string s)
+    string ans = "";
+    for (int i = 0; i < s.length();)
     {
-        this->s = s;
-    }
-    string freqAlphabets(string s)
-    {
-        string ans = "";
-        for (int i = 0; i < s.length();)
+        if (i + 2 < s.length() && s[i + 2] == '#')
         {
-            if (i + 2 < s.length() && s[i + 2] == '#')
-            {
-                int ss = (s[i] - '0') * 10 + (s[i + 1] - '0');
-                ans += char(ss + 'a' - 1);
-                i += 3;
-            }
-            else
-            {
-                ans += char(s[i] - '0' - 1 + 'a');
-                ++i;
-            }
+            int ss = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            ans += char(ss + 'a' - 1);
+            i += 3;
         }
-        return ans;
+        else
+        {
+            ans += char(s[i] - '0' - 1 + 'a');
+            ++i;
+        }
     }
-};
+    return ans;
+}
 int main()
 {
     string s = "10#11#12";
-    Solutioin s;
+    cout << freqAlphabets(s);
     return 0;
 }
