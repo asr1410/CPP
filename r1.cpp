d9eb7c03a887704e705afd93f1fc33f6c33005c4
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-12-02 11:38:53 +0530

    push

--- r1.cpp
+++ r1.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+class Solution
+{
+public:
+    int countKDifference(vector<int> &nums, int k)
+    {
+        int cnt[101] = {}, res = 0;
+        for (auto n : nums)
+            cnt[n]++;
+        for (int i = k + 1; i < 101; ++i)
+            res += cnt[i] * cnt[i - k];
+        return res;
+        // int count = 0;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     for (int j = i + 1; j < nums.size(); j++)
+        //     {
+        //         if (nums[i] - nums[j] == -k || nums[i] - nums[j] == k)
+        //         {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+    }
+};
 int main()
 {
-
+    vector<int> v = {3, 2, 1, 5, 4};
+    int k = 2;
+    Solution s;
+    cout << s.countKDifference(v, k) << endl;
     return 0;
 }
